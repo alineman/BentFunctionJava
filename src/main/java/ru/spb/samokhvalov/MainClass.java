ace39e776f9ef97026cb5e7c74bedb3ca7fcbc09
Ivan Samohvalov <isamohvalov@gmail.com>
2014-05-10 00:33:57 +0400

    [enhancement] First commit

---
+++ src/main/java/ru/spb/samokhvalov/MainClass.java
@@ -0,0 +1,15 @@
+package ru.spb.samokhvalov;
+
+
+import ru.spb.samokhvalov.anf.ANF;
+
+/**
+ * Created by ivan on 09.05.14.
+ */
+public class MainClass {
+
+    public static void main(String... args){
+        ANF variable = new ANF();
+        System.out.println("Hello, world!");
+    }
+}
---
+++ src/main/java/ru/spb/samokhvalov/anf/ANF.java
@@ -0,0 +1,10 @@
+package ru.spb.samokhvalov.anf;
+
+/**
+ * Created by ivan on 09.05.14.
+ */
+public class ANF {
+    public ANF() {
+        System.out.println("Success!");
+    }
+}
