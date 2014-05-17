a6520be2ee57644474689cc671134ad027db0fea
isamokhvalov <samokhvalov@iac.spb.ru>
2014-05-17 19:36:24 +0400

    [enhancement] add calculate fast Wals-Hadamar transform

--- src/main/java/ru/spb/samokhvalov/anf/ANF.java
+++ src/main/java/ru/spb/samokhvalov/anf/ANF.java
@@ -14,6 +14,7 @@ public class ANF {
     private long degree;
     private List<TableOfTrue> tableOfTrue;
     private int variables;
+    private int[] walshW;
 
     public ANF(long number, int variables) {
         this.number = number;
@@ -24,18 +25,18 @@ public class ANF {
         log.info("Success!");
     }
 
-    public ANF(Integer number){
+    public ANF(Integer number) {
         this.number = number;
         double deg = Math.ceil(Math.log(number) / Math.log(4));
         this.degree = Math.round(deg);
     }
 
-    public void printHexNumber(){
+    public void printHexNumber() {
         log.info("HexViev of number: " + Long.toHexString(number));
         log.info("degree: " + degree);
     }
 
-    public void printDecNumber(){
+    public void printDecNumber() {
         log.info("DecViev of number: " + number);
         log.info("degree: " + degree);
     }
@@ -48,10 +49,10 @@ public class ANF {
         return degree;
     }
 
-    public void fillTableOfTrue(){
+    public void fillTableOfTrue() {
         Long max = Long.rotateLeft(1, variables) - 1;
         tableOfTrue = new ArrayList<>();
-        for (long i = 0; i <= max; i++){
+        for (long i = 0; i <= max; i++) {
             tableOfTrue.add(new TableOfTrue(i, (number & (Long.rotateLeft(1, new Long(i).intValue()))) != 0, variables));
         }
     }
@@ -59,4 +60,44 @@ public class ANF {
     public List<TableOfTrue> getTableOfTrue() {
         return tableOfTrue;
     }
+
+    public void fillWalshW() {
+        int size = 1 << variables;
+        walshW = new int[size];
+        for (int i = 0; i < size; i++)
+            walshW[i] = ((number & (1 << i)) == 0 ? 1 : -1);
+        walshW = calculateFastHadamarTransform(walshW);
+    }
+
+
+    public int[] getWalshW() {
+        if (walshW == null)
+            fillWalshW();
+        return walshW;
+    }
+
+    private int[] calculateFastHadamarTransform(int[] input) {
+        if (input.length == 1)
+            return input;
+        int half = input.length >> 1;
+        int[] temp1 = new int[half];
+        int[] temp2 = new int[half];
+        for (int i = 0; i < half; i++) {
+            int half1 = input[i];
+            int half2 = input[i + half];
+            temp1[i] = half1 + half2;
+            temp2[i] = half1 - half2;
+
+        }
+        temp1 = calculateFastHadamarTransform(temp1);
+        temp2 = calculateFastHadamarTransform(temp2);
+        int[] result = new int[input.length];
+        for (int i = 0; i < half; i++) {
+            result[i] = temp1[i];
+            result[i + half] = temp2[i];
+        }
+
+        return result;
+    }
+
 }
--- src/test/java/ru/spb/samokhvalov/anf/ANFTest.java
+++ src/test/java/ru/spb/samokhvalov/anf/ANFTest.java
@@ -1,6 +1,7 @@
 package ru.spb.samokhvalov.anf;
 
 import lombok.extern.log4j.Log4j;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -8,18 +9,46 @@ import org.junit.Test;
  */
 @Log4j
 public class ANFTest {
+    private ANF anfNumber1;
     private static final long number1 = 2678982;
     private static final int variables1 = 5;
+    private int[] walshNumber1 = {
+            14, 6, 6, 6,
+            6, -2, -2, -2,
+            -6, 2, 2, 2,
+            -6, 2, 2, 2,
+            -10, -2, 6, 6,
+            6, -2, -10, 6,
+            2, -6, 2, 2,
+            -6, 2, -6, 10
+    };
+
+    private ANF anfNumber2;
     private static final long number2 = 30856;
     private static final int variables2 = 4;
+    private int[] walshNumber2 = {
+            4, 4, 4, -4,
+            4, 4, 4, -4,
+            4, 4, 4, -4,
+            -4, -4, -4, 4
+    };
+
+    @Before
+    public void prepareVariables() {
+        anfNumber2 = new ANF(number2, variables2);
+        anfNumber1 = new ANF(number1, variables1);
+        sortArray(walshNumber1);
+        sortArray(walshNumber2);
+
+    }
 
     @Test
-    public void simpleConstructor(){
+    public void simpleConstructor() {
         new ANF();
     }
 
     @Test
-    public void hexConstructor(){
+    public void hexConstructor() {
         ANF variable = new ANF(0xff);
         variable.printHexNumber();
     }
@@ -30,18 +59,17 @@ public class ANFTest {
         log.info(Math.ceil(1.5));
         for (int i = 0; i <= 255; i++) {
             ANF testValue = new ANF(i);
-            log.info("i: " + i + " degree: " + Math.pow(2,testValue.getDegree()));
+            log.info("i: " + i + " degree: " + Math.pow(2, testValue.getDegree()));
         }
 
     }
 
     @Test
-    public void testFillTableOfTrue(){
-        ANF anf = new ANF(number1,variables1);
-        long resultNumber=0;
-        anf.fillTableOfTrue();
+    public void testFillTableOfTrue() {
+        long resultNumber = 0;
+        anfNumber1.fillTableOfTrue();
         int i = 0;
-        for (TableOfTrue t: anf.getTableOfTrue()){
+        for (TableOfTrue t : anfNumber1.getTableOfTrue()) {
             int value = (t.getValue()) ? 1 : 0;
             log.info(t.getVariableString() + " | " + value);
             resultNumber += value << i;
@@ -50,11 +78,10 @@ public class ANFTest {
         log.info(resultNumber);
         assert number1 == resultNumber;
 
-         anf = new ANF(number2,variables2);
-        resultNumber=0;
-        anf.fillTableOfTrue();
+        resultNumber = 0;
+        anfNumber2.fillTableOfTrue();
         i = 0;
-        for (TableOfTrue t: anf.getTableOfTrue()){
+        for (TableOfTrue t : anfNumber2.getTableOfTrue()) {
             int value = (t.getValue()) ? 1 : 0;
             log.info(t.getVariableString() + " | " + value);
             resultNumber += value << i;
@@ -65,4 +92,39 @@ public class ANFTest {
 
     }
 
+    @Test
+    public void testFillWalshW() {
+        int length1 = anfNumber1.getWalshW().length;
+        int length2 = anfNumber2.getWalshW().length;
+        int[] walshW1 = new int[length1];
+        int[] walshW2 = new int[length2];
+        System.arraycopy(anfNumber1.getWalshW(), 0, walshW1, 0, length1);
+        System.arraycopy(anfNumber2.getWalshW(), 0, walshW2, 0, length2);
+        sortArray(walshW1);
+        sortArray(walshW2);
+        for (int i = 0; i < length1; i++) {
+            assert walshW1[i] == walshNumber1[i];
+        }
+        for (int i = 0; i < length2; i++) {
+            assert walshW2[i] == walshNumber2[i];
+        }
+    }
+
+
+    private void sortArray(int[] input) {
+        for (int i = input.length - 1; i >= 2; i--) {
+            boolean sorted = true;
+            for (int j = 0; j < i; j++) {
+                if (input[j] > input[j + 1]) {
+                    int temp = input[j];
+                    input[j] = input[j + 1];
+                    input[j + 1] = temp;
+                    sorted = false;
+                }
+            }
+            if (sorted) {
+                break;
+            }
+        }
+    }
 }
